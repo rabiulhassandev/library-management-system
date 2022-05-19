3e8a089dd9a6c7a5731d7b5f91db9bbddc8868d4
MD-Rabiull-Hassan <mdrabiullhassan@gmail.com>
2022-05-20 02:10:06 +0600

    about us card style fix

--- resources/views/pages/front/about-us.blade.php
+++ resources/views/pages/front/about-us.blade.php
@@ -18,191 +18,136 @@
 
 
         {{-- advisor-profiles Cards --}}
+        @isset($collection['advisor-profiles'])
+        @if(count($collection['advisor-profiles']) > 0)
         <div class="container py-3">
-            <div class="card shadow border-0" style="border-radius: 0px;">
-                <div class="card-header border-0 bg-primary">
-                    <h3 class="text-white">এডভাইজার</h3>
-                </div>
-                <div class="card-body">
-                    @isset($collection['advisor-profiles'])
-                    @if(count($collection['advisor-profiles']) > 0)
-                    <div class="row">
-                        @foreach ($collection['advisor-profiles'] as $profile)
-                        <div class="col-lg-3 col-md-4 col-sm-6 mt-2">
-                            <div class="card member-card shadow border-0">
-                                <div class="card-body">
-                                    <img src="{{ image_url($profile->image) }}">
-                                    <h4>{{ $profile->name }}</h4>
-                                    <h6 class="m-0 font-italic">{{ $profile->designation }}</h6>
-                                    <small class="m-0">{{ $profile->email }}</small><br>
-                                    <small class="m-0">{{ $profile->phone }}</small><br>
-                                    <small>{{ $profile->address }}</small>
-                                </div>
+            <h3 class="text-primary pb-2">এডভাইজার</h3>
+            <div class="row">
+                @foreach ($collection['advisor-profiles'] as $profile)
+                <div class="col-lg-3 col-md-4 col-sm-6 mt-2">
+                    <div class="card member-card shadow py-3" style="border: 0px; border-top: 5px solid #079c86 ;">
+                        <div class="card-body">
+                            <img src="{{ image_url($profile->image) }}" alt="{{ $profile->name }}">
+                            <div style="margin-top: -20px; padding-bottom: 20px;">
+                                <a href="#"><span class="bx bx-plus"></span></a>
                             </div>
+                            <a href="#"><h4>{{ $profile->name }}</h4></a>
+                            <h6 class="m-0 text-secondary">{{ $profile->designation }}</h6>
                         </div>
-                        @endforeach
                     </div>
-                    @else
-                    <div class="py-5 text-center">
-                        <i class='bx bx-file-find text-secondary' style="font-size: 50px"></i>
-                        <h3 class="text-primary m-0">কোনো তথ্য খুজে পাওয়া যায় নি</h3>
-                    </div>
-                    @endif
-                    @endisset
                 </div>
+                @endforeach
             </div>
         </div>
+        @endif
+        @endisset
         {{-- advisor Cards --}}
 
         {{-- mentor Cards --}}
+        @isset($collection['mentor-profiles'])
+        @if(count($collection['mentor-profiles']) > 0)
         <div class="container py-3">
-            <div class="card shadow border-0" style="border-radius: 0px;">
-                <div class="card-header border-0 bg-primary">
-                    <h3 class="text-white">মেন্টর্স</h3>
-                </div>
-                <div class="card-body">
-                    @isset($collection['mentor-profiles'])
-                    @if(count($collection['mentor-profiles']) > 0)
-                    <div class="row">
-                        @foreach ($collection['mentor-profiles'] as $profile)
-                        <div class="col-lg-3 col-md-4 col-sm-6 mt-2">
-                            <div class="card member-card shadow border-0">
-                                <div class="card-body">
-                                    <img src="{{ image_url($profile->image) }}">
-                                    <h4>{{ $profile->name }}</h4>
-                                    <h6 class="m-0 font-italic">{{ $profile->designation }}</h6>
-                                    <small class="m-0">{{ $profile->email }}</small><br>
-                                    <small class="m-0">{{ $profile->phone }}</small><br>
-                                    <small>{{ $profile->address }}</small>
-                                </div>
+            <h3 class="text-primary pb-2">মেন্টর্স</h3>
+            <div class="row">
+                @foreach ($collection['mentor-profiles'] as $profile)
+                <div class="col-lg-3 col-md-4 col-sm-6 mt-2">
+                    <div class="card member-card shadow py-3" style="border: 0px; border-top: 5px solid #079c86 ;">
+                        <div class="card-body">
+                            <img src="{{ image_url($profile->image) }}" alt="{{ $profile->name }}">
+                            <div style="margin-top: -20px; padding-bottom: 20px;">
+                                <a href="#"><span class="bx bx-plus"></span></a>
                             </div>
+                            <a href="#"><h4>{{ $profile->name }}</h4></a>
+                            <h6 class="m-0 text-secondary">{{ $profile->designation }}</h6>
                         </div>
-                        @endforeach
                     </div>
-                    @else
-                    <div class="py-5 text-center">
-                        <i class='bx bx-file-find text-secondary' style="font-size: 50px"></i>
-                        <h3 class="text-primary m-0">কোনো তথ্য খুজে পাওয়া যায় নি</h3>
-                    </div>
-                    @endif
-                    @endisset
                 </div>
+                @endforeach
             </div>
         </div>
+        @endif
+        @endisset
         {{-- mentor Cards --}}
 
         {{-- founder Cards --}}
+        @isset($collection['founder-profiles'])
+        @if(count($collection['founder-profiles']) > 0)
         <div class="container py-3">
-            <div class="card shadow border-0" style="border-radius: 0px;">
-                <div class="card-header border-0 bg-primary">
-                    <h3 class="text-white">ফাউন্ডার টিম</h3>
-                </div>
-                <div class="card-body">
-                    @isset($collection['founder-profiles'])
-                    @if(count($collection['founder-profiles']) > 0)
-                    <div class="row">
-                        @foreach ($collection['founder-profiles'] as $profile)
-                        <div class="col-lg-3 col-md-4 col-sm-6 mt-2">
-                            <div class="card member-card shadow border-0">
-                                <div class="card-body">
-                                    <img src="{{ image_url($profile->image) }}">
-                                    <h4>{{ $profile->name }}</h4>
-                                    <h6 class="m-0 font-italic">{{ $profile->designation }}</h6>
-                                    <small class="m-0">{{ $profile->email }}</small><br>
-                                    <small class="m-0">{{ $profile->phone }}</small><br>
-                                    <small>{{ $profile->address }}</small>
-                                </div>
+            <h3 class="text-primary pb-2">ফাউন্ডার টিম</h3>
+            <div class="row">
+                @foreach ($collection['founder-profiles'] as $profile)
+                <div class="col-lg-3 col-md-4 col-sm-6 mt-2">
+                    <div class="card member-card shadow py-3" style="border: 0px; border-top: 5px solid #079c86 ;">
+                        <div class="card-body">
+                            <img src="{{ image_url($profile->image) }}" alt="{{ $profile->name }}">
+                            <div style="margin-top: -20px; padding-bottom: 20px;">
+                                <a href="#"><span class="bx bx-plus"></span></a>
                             </div>
+                            <a href="#"><h4>{{ $profile->name }}</h4></a>
+                            <h6 class="m-0 text-secondary">{{ $profile->designation }}</h6>
                         </div>
-                        @endforeach
                     </div>
-                    @else
-                    <div class="py-5 text-center">
-                        <i class='bx bx-file-find text-secondary' style="font-size: 50px"></i>
-                        <h3 class="text-primary m-0">কোনো তথ্য খুজে পাওয়া যায় নি</h3>
-                    </div>
-                    @endif
-                    @endisset
                 </div>
+                @endforeach
             </div>
         </div>
+        @endif
+        @endisset
         {{-- founder Cards --}}
 
         {{-- volunteer Cards --}}
+        @isset($collection['volunteer-profiles'])
+        @if(count($collection['volunteer-profiles']) > 0)
         <div class="container py-3">
-            <div class="card shadow border-0" style="border-radius: 0px;">
-                <div class="card-header border-0 bg-primary">
-                    <h3 class="text-white">স্বেচ্ছাসেবী</h3>
-                </div>
-                <div class="card-body">
-                    @isset($collection['volunteer-profiles'])
-                    @if(count($collection['volunteer-profiles']) > 0)
-                    <div class="row">
-                        @foreach ($collection['volunteer-profiles'] as $profile)
-                        <div class="col-lg-3 col-md-4 col-sm-6 mt-2">
-                            <div class="card member-card shadow border-0">
-                                <div class="card-body">
-                                    <img src="{{ image_url($profile->image) }}">
-                                    <h4>{{ $profile->name }}</h4>
-                                    <h6 class="m-0 font-italic">{{ $profile->designation }}</h6>
-                                    <small class="m-0">{{ $profile->email }}</small><br>
-                                    <small class="m-0">{{ $profile->phone }}</small><br>
-                                    <small>{{ $profile->address }}</small>
-                                </div>
+            <h3 class="text-primary pb-2">স্বেচ্ছাসেবী</h3>
+            <div class="row">
+                @foreach ($collection['volunteer-profiles'] as $profile)
+                <div class="col-lg-3 col-md-4 col-sm-6 mt-2">
+                    <div class="card member-card shadow py-3" style="border: 0px; border-top: 5px solid #079c86 ;">
+                        <div class="card-body">
+                            <img src="{{ image_url($profile->image) }}" alt="{{ $profile->name }}">
+                            <div style="margin-top: -20px; padding-bottom: 20px;">
+                                <a href="#"><span class="bx bx-plus"></span></a>
                             </div>
+                            <a href="#"><h4>{{ $profile->name }}</h4></a>
+                            <h6 class="m-0 text-secondary">{{ $profile->designation }}</h6>
                         </div>
-                        @endforeach
                     </div>
-                    @else
-                    <div class="py-5 text-center">
-                        <i class='bx bx-file-find text-secondary' style="font-size: 50px"></i>
-                        <h3 class="text-primary m-0">কোনো তথ্য খুজে পাওয়া যায় নি</h3>
-                    </div>
-                    @endif
-                    @endisset
                 </div>
+                @endforeach
             </div>
         </div>
+        @endif
+        @endisset
         {{-- volunteer Cards --}}
 
 
 
 
         {{-- campus-member Cards --}}
+        @isset($collection['campus-representative-profiles'])
+        @if(count($collection['campus-representative-profiles']) > 0)
         <div class="container py-3">
-            <div class="card shadow border-0" style="border-radius: 0px;">
-                <div class="card-header border-0 bg-primary">
-                    <h3 class="text-white">ক্যাম্পাস প্রতিনিধি</h3>
-                </div>
-                <div class="card-body">
-                    @isset($collection['campus-representative-profiles'])
-                    @if(count($collection['campus-representative-profiles']) > 0)
-                    <div class="row">
-                        @foreach ($collection['campus-representative-profiles'] as $profile)
-                        <div class="col-lg-3 col-md-4 col-sm-6 mt-2">
-                            <div class="card member-card shadow border-0">
-                                <div class="card-body">
-                                    <img src="{{ image_url($profile->image) }}">
-                                    <h4>{{ $profile->name }}</h4>
-                                    <h6 class="m-0 font-italic">{{ $profile->designation }}</h6>
-                                    <small class="m-0">{{ $profile->email }}</small><br>
-                                    <small class="m-0">{{ $profile->phone }}</small><br>
-                                    <small>{{ $profile->address }}</small>
-                                </div>
+            <h3 class="text-primary pb-2">ক্যাম্পাস প্রতিনিধি</h3>
+            <div class="row">
+                @foreach ($collection['campus-representative-profiles'] as $profile)
+                <div class="col-lg-3 col-md-4 col-sm-6 mt-2">
+                    <div class="card member-card shadow py-3" style="border: 0px; border-top: 5px solid #079c86 ;">
+                        <div class="card-body">
+                            <img src="{{ image_url($profile->image) }}" alt="{{ $profile->name }}">
+                            <div style="margin-top: -20px; padding-bottom: 20px;">
+                                <a href="#"><span class="bx bx-plus"></span></a>
                             </div>
+                            <a href="#"><h4>{{ $profile->name }}</h4></a>
+                            <h6 class="m-0 text-secondary">{{ $profile->designation }}</h6>
                         </div>
-                        @endforeach
                     </div>
-                    @else
-                    <div class="py-5 text-center">
-                        <i class='bx bx-file-find text-secondary' style="font-size: 50px"></i>
-                        <h3 class="text-primary m-0">কোনো তথ্য খুজে পাওয়া যায় নি</h3>
-                    </div>
-                    @endif
-                    @endisset
                 </div>
+                @endforeach
             </div>
         </div>
+        @endif
+        @endisset
         {{-- campus-member Cards --}}
 
 
